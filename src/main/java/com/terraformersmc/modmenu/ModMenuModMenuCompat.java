ookkoouu/ModMenu

Add provided update checkers, loader update checkers (#711)
- Add update checkers for Fabric Loader and Quilt Loader
- Add API for mods to provide update checkers for other mods

src/main/java/com/terraformersmc/modmenu/ModMenuModMenuCompat.java
@@ -1,16 +1,18 @@
 package com.terraformersmc.modmenu;
 
-import com.google.common.collect.ImmutableMap;
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
+import com.terraformersmc.modmenu.api.UpdateChecker;
 import com.terraformersmc.modmenu.gui.ModMenuOptionsScreen;
+import com.terraformersmc.modmenu.util.mod.fabric.FabricLoaderUpdateChecker;
+import com.terraformersmc.modmenu.util.mod.quilt.QuiltLoaderUpdateChecker;
+
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.option.OptionsScreen;
 
 import java.util.Map;
 
 public class ModMenuModMenuCompat implements ModMenuApi {
-
 	@Override
 	public ConfigScreenFactory<?> getModConfigScreenFactory() {
 		return ModMenuOptionsScreen::new;
@@ -18,6 +20,15 @@ public class ModMenuModMenuCompat implements ModMenuApi {
 
 	@Override
 	public Map<String, ConfigScreenFactory<?>> getProvidedConfigScreenFactories() {
-		return ImmutableMap.of("minecraft", parent -> new OptionsScreen(parent, MinecraftClient.getInstance().options));
+		return Map.of("minecraft", parent -> new OptionsScreen(parent, MinecraftClient.getInstance().options));
+	}
+
+	@Override
+	public Map<String, UpdateChecker> getProvidedUpdateCheckers() {
+		if (ModMenu.runningQuilt) {
+			return Map.of("quilt_loader", new QuiltLoaderUpdateChecker());
+		} else {
+			return Map.of("fabricloader", new FabricLoaderUpdateChecker());
+		}
 	}
 }

src/main/java/com/terraformersmc/modmenu/api/ModMenuApi.java
@@ -1,6 +1,5 @@
 package com.terraformersmc.modmenu.api;
 
-import com.google.common.collect.ImmutableMap;
 import com.terraformersmc.modmenu.ModMenu;
 import com.terraformersmc.modmenu.gui.ModsScreen;
 import net.minecraft.client.gui.screen.Screen;
@@ -66,7 +65,17 @@ public interface ModMenuApi {
 	 * @return a map of mod ids to screen factories.
 	 */
 	default Map<String, ConfigScreenFactory<?>> getProvidedConfigScreenFactories() {
-		return ImmutableMap.of();
+		return Map.of();
+	}
+
+	/**
+	 * Used to provide update checkers for other mods. A mod registering its own
+	 * update checker will take priority over any provided ones should both exist.
+	 *
+	 * @return a map of mod ids to update checkers.
+	 */
+	default Map<String, UpdateChecker> getProvidedUpdateCheckers() {
+		return Map.of();
 	}
 
 	/**

LostLuma/modmenu-ornithe

src/main/java/com/terraformersmc/modmenu/ModMenuModMenuCompat.java
@@ -3,14 +3,17 @@ package com.terraformersmc.modmenu;
 import com.google.common.collect.ImmutableMap;
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
+import com.terraformersmc.modmenu.api.UpdateChecker;
 import com.terraformersmc.modmenu.gui.ModMenuOptionsScreen;
+import com.terraformersmc.modmenu.util.mod.fabric.FabricLoaderUpdateChecker;
+import com.terraformersmc.modmenu.util.mod.quilt.QuiltLoaderUpdateChecker;
+
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screen.options.OptionsScreen;
 
 import java.util.Map;
 
 public class ModMenuModMenuCompat implements ModMenuApi {
-
 	@Override
 	public ConfigScreenFactory<?> getModConfigScreenFactory() {
 		return ModMenuOptionsScreen::new;
@@ -19,5 +22,14 @@ public class ModMenuModMenuCompat implements ModMenuApi {
 	@Override
 	public Map<String, ConfigScreenFactory<?>> getProvidedConfigScreenFactories() {
 		return ImmutableMap.of("minecraft", parent -> new OptionsScreen(parent, Minecraft.getInstance().options));
+	}
+
+	@Override
+	public Map<String, UpdateChecker> getProvidedUpdateCheckers() {
+		if (ModMenu.runningQuilt) {
+			return ImmutableMap.of("quilt_loader", new QuiltLoaderUpdateChecker());
+		} else {
+			return ImmutableMap.of("fabricloader", new FabricLoaderUpdateChecker());
+		}
 	}
 }

src/main/java/com/terraformersmc/modmenu/api/ModMenuApi.java
@@ -70,6 +70,16 @@ public interface ModMenuApi {
 	}
 
 	/**
+	 * Used to provide update checkers for other mods. A mod registering its own
+	 * update checker will take priority over any provided ones should both exist.
+	 *
+	 * @return a map of mod ids to update checkers.
+	 */
+	default Map<String, UpdateChecker> getProvidedUpdateCheckers() {
+		return ImmutableMap.of();
+	}
+
+	/**
 	 * Used to mark mods with a badge indicating that they are
 	 * provided by a modpack.
 	 * <p>