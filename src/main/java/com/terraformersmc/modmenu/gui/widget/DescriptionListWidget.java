notlin4/ModMenu

Fix update checker promoting downgrades, other followup improvements (#716)
- Fixed update checker promoting downgrades

src/main/java/com/terraformersmc/modmenu/gui/widget/DescriptionListWidget.java
@@ -37,7 +37,6 @@ public class DescriptionListWidget extends EntryListWidget<DescriptionListWidget
 
 	private static final Text HAS_UPDATE_TEXT = Text.translatable("modmenu.hasUpdate");
 	private static final Text EXPERIMENTAL_TEXT = Text.translatable("modmenu.experimental").formatted(Formatting.GOLD);
-	private static final Text MODRINTH_TEXT = Text.translatable("modmenu.modrinth");
 	private static final Text DOWNLOAD_TEXT = Text.translatable("modmenu.downloadLink").formatted(Formatting.BLUE).formatted(Formatting.UNDERLINE);
 	private static final Text CHILD_HAS_UPDATE_TEXT = Text.translatable("modmenu.childHasUpdate");
 	private static final Text LINKS_TEXT = Text.translatable("modmenu.links");
@@ -114,30 +113,20 @@ public class DescriptionListWidget extends EntryListWidget<DescriptionListWidget
 							children().add(new DescriptionEntry(line, 8));
 						}
 
-						if (updateInfo instanceof ModrinthUpdateInfo modrinthUpdateInfo) {
-							Text updateText = Text.translatable("modmenu.updateText", VersionUtil.stripPrefix(modrinthUpdateInfo.getVersionNumber()), MODRINTH_TEXT)
-								.formatted(Formatting.BLUE)
-								.formatted(Formatting.UNDERLINE);
-
-							for (OrderedText line : textRenderer.wrapLines(updateText, wrapWidth - 16)) {
-								children().add(new LinkEntry(line, modrinthUpdateInfo.getDownloadLink(), 8));
-							}
+						Text updateMessage = updateInfo.getUpdateMessage();
+						String downloadLink = updateInfo.getDownloadLink();
+						if (updateMessage == null) {
+							updateMessage = DOWNLOAD_TEXT;
 						} else {
-							Text updateMessage = updateInfo.getUpdateMessage();
-							String downloadLink = updateInfo.getDownloadLink();
-							if (updateMessage == null) {
-								updateMessage = DOWNLOAD_TEXT;
-							} else {
-								if (downloadLink != null) {
-									updateMessage = updateMessage.copy().formatted(Formatting.BLUE).formatted(Formatting.UNDERLINE);
-								}
+							if (downloadLink != null) {
+								updateMessage = updateMessage.copy().formatted(Formatting.BLUE).formatted(Formatting.UNDERLINE);
 							}
-							for (OrderedText line : textRenderer.wrapLines(updateMessage, wrapWidth - 16)) {
-								if (downloadLink != null) {
-									children().add(new LinkEntry(line, downloadLink, 8));
-								} else {
-									children().add(new DescriptionEntry(line, 8));
-								}
+						}
+						for (OrderedText line : textRenderer.wrapLines(updateMessage, wrapWidth - 16)) {
+							if (downloadLink != null) {
+								children().add(new LinkEntry(line, downloadLink, 8));
+							} else {
+								children().add(new DescriptionEntry(line, 8));
 							}
 						}
 					}

src/main/java/com/terraformersmc/modmenu/util/mod/ModrinthUpdateInfo.java
@@ -1,13 +1,20 @@
 package com.terraformersmc.modmenu.util.mod;
 
+import org.jetbrains.annotations.Nullable;
+
 import com.terraformersmc.modmenu.api.UpdateChannel;
 import com.terraformersmc.modmenu.api.UpdateInfo;
+import com.terraformersmc.modmenu.util.VersionUtil;
+
+import net.minecraft.text.Text;
 
 public class ModrinthUpdateInfo implements UpdateInfo {
 	protected final String projectId;
 	protected final String versionId;
 	protected final String versionNumber;
 	protected final UpdateChannel updateChannel;
+
+	private static final Text MODRINTH_TEXT = Text.translatable("modmenu.modrinth");
 
 	public ModrinthUpdateInfo(String projectId, String versionId, String versionNumber, UpdateChannel updateChannel) {
 		this.projectId = projectId;
@@ -19,6 +26,11 @@ public class ModrinthUpdateInfo implements UpdateInfo {
 	@Override
 	public boolean isUpdateAvailable() {
 		return true;
+	}
+
+	@Override
+	public @Nullable Text getUpdateMessage() {
+		return Text.translatable("modmenu.updateText", VersionUtil.stripPrefix(this.versionNumber), MODRINTH_TEXT);
 	}
 
 	@Override

src/main/java/com/terraformersmc/modmenu/util/mod/quilt/QuiltLoaderUpdateChecker.java
@@ -5,6 +5,7 @@ import java.net.URI;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
+import org.jetbrains.annotations.Nullable;
 import org.quiltmc.loader.api.QuiltLoader;
 import org.quiltmc.loader.api.Version;
 import org.quiltmc.loader.api.VersionFormatException;
@@ -17,6 +18,8 @@ import com.terraformersmc.modmenu.api.UpdateChecker;
 import com.terraformersmc.modmenu.api.UpdateInfo;
 import com.terraformersmc.modmenu.util.HttpUtil;
 import com.terraformersmc.modmenu.util.JsonUtil;
+
+import net.minecraft.text.Text;
 
 public class QuiltLoaderUpdateChecker implements UpdateChecker {
 	public static final Logger LOGGER = LoggerFactory.getLogger("Mod Menu/Quilt Update Checker");
@@ -105,19 +108,7 @@ public class QuiltLoaderUpdateChecker implements UpdateChecker {
 		}
 
 		LOGGER.debug("Quilt Loader has a matching update available!");
-
-		UpdateChannel updateChannel;
-		var preRelease = match.preRelease();
-
-		if (preRelease.isEmpty()) {
-			updateChannel = UpdateChannel.RELEASE;
-		} else if (isStableOrBeta(preRelease)) {
-			updateChannel = UpdateChannel.BETA;
-		} else {
-			updateChannel = UpdateChannel.ALPHA;
-		}
-
-		return new QuiltLoaderUpdateInfo(updateChannel);
+		return new QuiltLoaderUpdateInfo(match);
 	}
 
 	private static boolean isNewer(Version.Semantic self, Version.Semantic other) {
@@ -133,15 +124,20 @@ public class QuiltLoaderUpdateChecker implements UpdateChecker {
 	}
 
 	private static class QuiltLoaderUpdateInfo implements UpdateInfo {
-		private final UpdateChannel updateChannel;
+		private final Version.Semantic version;
 
-		private QuiltLoaderUpdateInfo(UpdateChannel updateChannel) {
-			this.updateChannel = updateChannel;
+		private QuiltLoaderUpdateInfo(Version.Semantic version) {
+			this.version = version;
 		}
 
 		@Override
 		public boolean isUpdateAvailable() {
 			return true;
+		}
+
+		@Override
+		public @Nullable Text getUpdateMessage() {
+			return Text.translatable("modmenu.install_version", this.version.raw());
 		}
 
 		@Override
@@ -151,7 +147,15 @@ public class QuiltLoaderUpdateChecker implements UpdateChecker {
 
 		@Override
 		public UpdateChannel getUpdateChannel() {
-			return this.updateChannel;
+			var preRelease = this.version.preRelease();
+
+			if (preRelease.isEmpty()) {
+				return UpdateChannel.RELEASE;
+			} else if (isStableOrBeta(preRelease)) {
+				return UpdateChannel.BETA;
+			} else {
+				return UpdateChannel.ALPHA;
+			}
 		}
 	}
 }

LostLuma/modmenu-ornithe

src/main/java/com/terraformersmc/modmenu/gui/widget/DescriptionListWidget.java
@@ -35,7 +35,6 @@ public class DescriptionListWidget extends EntryListWidget {
 
 	private static final String HAS_UPDATE_TEXT = I18n.translate("modmenu.hasUpdate");
 	private static final String EXPERIMENTAL_TEXT = /*Formatting.GOLD +*/ I18n.translate("modmenu.experimental");
-	private static final String MODRINTH_TEXT = I18n.translate("modmenu.modrinth");
 	private static final String DOWNLOAD_TEXT = "" + /*Formatting.BLUE + Formatting.UNDERLINE +*/ I18n.translate("modmenu.downloadLink");
 	private static final String CHILD_HAS_UPDATE_TEXT = I18n.translate("modmenu.childHasUpdate");
 	private static final String LINKS_TEXT = I18n.translate("modmenu.links");
@@ -125,29 +124,21 @@ public class DescriptionListWidget extends EntryListWidget {
 							this.entries.add(new DescriptionEntry((String) line, 8));
 						}
 
-						if (updateInfo instanceof ModrinthUpdateInfo) {
-							ModrinthUpdateInfo modrinthUpdateInfo = (ModrinthUpdateInfo) updateInfo;
-							String updateText = "" + /*Formatting.BLUE + Formatting.UNDERLINE +*/ I18n.translate("modmenu.updateText", VersionUtil.stripPrefix(modrinthUpdateInfo.getVersionNumber()), MODRINTH_TEXT);
-
-							for (Object line : textRenderer.split(updateText, wrapWidth - 16)) {
-								this.entries.add(new LinkEntry((String) line, modrinthUpdateInfo.getDownloadLink(), 8));
-							}
+						String updateMessage = updateInfo.getUpdateMessage();
+						String downloadLink = updateInfo.getDownloadLink();
+						if (updateMessage == null) {
+							updateMessage = DOWNLOAD_TEXT;
 						} else {
-							String updateMessage = updateInfo.getUpdateMessage();
-							String downloadLink = updateInfo.getDownloadLink();
-							if (updateMessage == null) {
-								updateMessage = DOWNLOAD_TEXT;
-							} else {
-								if (downloadLink != null) {
-									updateMessage = "" + /*Formatting.BLUE + Formatting.UNDERLINE +*/ updateMessage;
-								}
+							if (downloadLink != null) {
+								updateMessage = "" + /*Formatting.BLUE + Formatting.UNDERLINE +*/ updateMessage;
 							}
-							for (Object line : textRenderer.split(updateMessage, wrapWidth - 16)) {
-								if (downloadLink != null) {
-									this.entries.add(new LinkEntry((String) line, downloadLink, 8));
-								} else {
-									this.entries.add(new DescriptionEntry((String) line, 8));
-								}
+						}
+
+						for (Object line : textRenderer.split(updateMessage, wrapWidth - 16)) {
+							if (downloadLink != null) {
+								this.entries.add(new LinkEntry((String) line, downloadLink, 8));
+							} else {
+								this.entries.add(new DescriptionEntry((String) line, 8));
 							}
 						}
 					}

src/main/java/com/terraformersmc/modmenu/util/mod/ModrinthUpdateInfo.java
@@ -1,13 +1,20 @@
 package com.terraformersmc.modmenu.util.mod;
 
+import org.jetbrains.annotations.Nullable;
+
 import com.terraformersmc.modmenu.api.UpdateChannel;
 import com.terraformersmc.modmenu.api.UpdateInfo;
+import com.terraformersmc.modmenu.util.VersionUtil;
+
+import net.minecraft.client.resource.language.I18n;
 
 public class ModrinthUpdateInfo implements UpdateInfo {
 	protected final String projectId;
 	protected final String versionId;
 	protected final String versionNumber;
 	protected final UpdateChannel updateChannel;
+
+	private static final String MODRINTH_TEXT = I18n.translate("modmenu.modrinth");
 
 	public ModrinthUpdateInfo(String projectId, String versionId, String versionNumber, UpdateChannel updateChannel) {
 		this.projectId = projectId;
@@ -19,6 +26,11 @@ public class ModrinthUpdateInfo implements UpdateInfo {
 	@Override
 	public boolean isUpdateAvailable() {
 		return true;
+	}
+
+	@Override
+	public @Nullable String getUpdateMessage() {
+		return I18n.translate("modmenu.updateText", VersionUtil.stripPrefix(this.versionNumber), MODRINTH_TEXT);
 	}
 
 	@Override

src/main/java/com/terraformersmc/modmenu/util/mod/quilt/QuiltLoaderUpdateChecker.java
@@ -10,6 +10,7 @@ import org.apache.http.client.methods.RequestBuilder;
 import org.apache.http.util.EntityUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.jetbrains.annotations.Nullable;
 import org.quiltmc.loader.api.QuiltLoader;
 import org.quiltmc.loader.api.Version;
 import org.quiltmc.loader.api.VersionFormatException;
@@ -22,6 +23,8 @@ import com.terraformersmc.modmenu.api.UpdateChecker;
 import com.terraformersmc.modmenu.api.UpdateInfo;
 import com.terraformersmc.modmenu.util.HttpUtil;
 import com.terraformersmc.modmenu.util.JsonUtil;
+
+import net.minecraft.client.resource.language.I18n;
 
 public class QuiltLoaderUpdateChecker implements UpdateChecker {
 	public static final Logger LOGGER = LogManager.getLogger("Mod Menu/Quilt Update Checker");
@@ -110,19 +113,7 @@ public class QuiltLoaderUpdateChecker implements UpdateChecker {
 		}
 
 		LOGGER.debug("Quilt Loader has a matching update available!");
-
-		UpdateChannel updateChannel;
-		String preRelease = match.preRelease();
-
-		if (preRelease.isEmpty()) {
-			updateChannel = UpdateChannel.RELEASE;
-		} else if (isStableOrBeta(preRelease)) {
-			updateChannel = UpdateChannel.BETA;
-		} else {
-			updateChannel = UpdateChannel.ALPHA;
-		}
-
-		return new QuiltLoaderUpdateInfo(updateChannel);
+		return new QuiltLoaderUpdateInfo(match);
 	}
 
 	private static boolean isNewer(Version.Semantic self, Version.Semantic other) {
@@ -138,15 +129,20 @@ public class QuiltLoaderUpdateChecker implements UpdateChecker {
 	}
 
 	private static class QuiltLoaderUpdateInfo implements UpdateInfo {
-		private final UpdateChannel updateChannel;
+		private final Version.Semantic version;
 
-		private QuiltLoaderUpdateInfo(UpdateChannel updateChannel) {
-			this.updateChannel = updateChannel;
+		private QuiltLoaderUpdateInfo(Version.Semantic version) {
+			this.version = version;
 		}
 
 		@Override
 		public boolean isUpdateAvailable() {
 			return true;
+		}
+
+		@Override
+		public @Nullable String getUpdateMessage() {
+			return I18n.translate("modmenu.install_version", this.version.raw());
 		}
 
 		@Override
@@ -156,7 +152,15 @@ public class QuiltLoaderUpdateChecker implements UpdateChecker {
 
 		@Override
 		public UpdateChannel getUpdateChannel() {
-			return this.updateChannel;
+			String preRelease = this.version.preRelease();
+
+			if (preRelease.isEmpty()) {
+				return UpdateChannel.RELEASE;
+			} else if (isStableOrBeta(preRelease)) {
+				return UpdateChannel.BETA;
+			} else {
+				return UpdateChannel.ALPHA;
+			}
 		}
 	}
 }